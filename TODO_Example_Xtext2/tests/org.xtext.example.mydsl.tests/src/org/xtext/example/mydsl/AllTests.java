e4e53929dbdab3e3b67c1d48f83a6ff71676c1fa
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2012-04-30 15:47:41 +0200

    added test suite

---
+++ TODO_Example_Xtext2/tests/org.xtext.example.mydsl.tests/src/org/xtext/example/mydsl/AllTests.java
@@ -0,0 +1,11 @@
+package org.xtext.example.mydsl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MyDslTest.class })
+public class AllTests {
+
+}
