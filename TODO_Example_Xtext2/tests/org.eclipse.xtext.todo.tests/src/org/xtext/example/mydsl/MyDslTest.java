a3cc3fa55b8ab3de88a53a81a8c9b1206261a46b
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2013-09-08 02:25:12 +0200

    Updated to Xtext 2.4.3

--- TODO_Example_Xtext2/tests/org.eclipse.xtext.todo.tests/src/org/xtext/example/mydsl/MyDslTest.java
+++ TODO_Example_Xtext2/tests/org.eclipse.xtext.todo.tests/src/org/xtext/example/mydsl/MyDslTest.java
@@ -1,17 +1,58 @@
 package org.xtext.example.mydsl;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 import org.eclipse.xtext.junit4.InjectWith;
-import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
-import org.eclipselabs.xtext.utils.unittesting.XtextTest;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.eclipse.xtext.junit4.util.ParseHelper;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.xtext.example.mydsl.myDsl.Model;
 
-@RunWith(XtextRunner2.class)
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.google.inject.Inject;
+
+@RunWith(XtextRunner.class)
 @InjectWith(MyDslInjectorProvider.class)
-public class MyDslTest extends XtextTest {
+public class MyDslTest {
+	@Inject
+	XtextResourceSet rs;
+	@Inject
+	ParseHelper<?> parseHelper;
 
-    @Test
-    public void testMydsl() {
-        testFileNoSerializer("test.mydsl");
-    }
+	@Test
+	public void testMydsl() throws Exception {
+		File file = new File("res/test.mydsl");
+		// Resource res =
+		// rs.getResource(URI.createFileURI(file.getAbsolutePath()), true);
+		// Map<String, String> options = new HashMap<String, String>();
+		// options.put(XMLResource.OPTION_ENCODING, "UTF-8");
+		// res.load(options);
+		// Assert.assertEquals("errors: " + res.getErrors(),
+		// 0,res.getErrors().size());
+		// Assert.assertEquals("root element exists", 1,
+		// res.getContents().size());
+		// Model m = (Model) res.getContents().get(0);
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+			String stringFromStream = CharStreams
+					.toString(new InputStreamReader(fis, Charsets.UTF_8));
+			Model model = (Model) parseHelper.parse(stringFromStream);
+			Assert.assertNotNull("model null", model);
+			Assert.assertEquals("errors: " + model.eResource().getErrors(), 0,
+					model.eResource().getErrors().size());
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		} finally {
+			if (fis != null)
+				fis.close();
+		}
+	}
 }
